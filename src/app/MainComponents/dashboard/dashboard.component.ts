a2c0a87f8e226c4384acbd7c5bda2e765fd954cd
Yash Paudel <80587185+h4syy@users.noreply.github.com>
2022-09-19 01:00:37 +0545

    Update dashboard.component.ts

--- src/app/MainComponents/dashboard/dashboard.component.ts
+++ src/app/MainComponents/dashboard/dashboard.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +6,8 @@ import {Router} from "@angular/router";
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-  constructor(private breakpointObserver: BreakpointObserver, private router: Router) { }
+
+  constructor() { }
 
   ngOnInit(): void {
   }
